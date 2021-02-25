55f3638f76d6fa779b1e14ab29c4f5c6367d9ef1
isl <isl9702@naver.com>
2021-02-25 18:06:02 +0900

    0225-3 결과 가져오기

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Result(){
+    const [codes, setCodes] = useState();
+    
+    async function fetch() {
+        const url = "https://cors-anywhere.herokuapp.com/https://inspct.career.go.kr/web/psycho/value/report?seq=NTI4NzU4NDQ";
+        const response = await (await axios.get(url)).data;
+        console.log(typeof(response));
+        console.log(response);
+        setCodes(response);
+    }
+
+    useEffect(() => {
+        fetch();
+    }, []);
+
     return(
+        <>
         <h1>검사가 완료되었습니다!</h1>
+        <div dangerouslySetInnerHTML={{__html: codes}}></div>
+        </>
     )
 }
